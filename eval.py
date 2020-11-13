5082bb9516bb5d8d79fe18e3603b29dd1cec2492
Rasmus Lindahl <mrconter1@gmail.com>
2020-11-13 14:28:10 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -43,6 +43,30 @@ def im_detect(net, target_data,im_data, im_info, features_given=True):
 
     return scores, boxes
 
+import random
+def load_image():
+
+  pathToFolder = '/content/drive/My Drive/Data/SyntheticDataset/'
+
+  f=open(pathToFolder + "info.csv")
+  lines=f.readlines()
+
+  data = random.choice(lines).split("\t")
+  image_name = data[0]
+  target_name_1 = data[5]
+  target_name_2 = data[6]
+
+  pre_load_image = cv2.imread(os.path.join(pathToFolder, "Data", image_name))
+  pre_load_target_1 = cv2.imread(os.path.join(pathToFolder, "Data", target_name_1))
+  pre_load_target_2 = cv2.imread(os.path.join(pathToFolder, "Data", target_name_2))
+
+  image = cv2.resize(pre_load_image, (1920, 1080), interpolation = cv2.INTER_AREA)
+  bbox = [int(data[1]), int(data[2]), int(data[3]), int(data[4]), 1]
+  target1 = cv2.resize(pre_load_target_1, (80, 80), interpolation = cv2.INTER_AREA)
+  target2 = cv2.resize(pre_load_target_2, (80, 80), interpolation = cv2.INTER_AREA)
+
+  return image, bbox, target1, target2
+
 if __name__ == '__main__':
 
     #load config file
@@ -57,6 +81,30 @@ if __name__ == '__main__':
     net.features.eval()#freeze batchnorms layers?
     print('load model successfully!')
     
-    net.cuda()
+    #net.cuda()
     net.eval()
+
+    batch_im_data = []
+    batch_target_data = []
+    batch_gt_boxes = []
+
+    im_data, gt_boxes, target1, target2 = load_image()
+
+    batch_im_data.append(normalize_image(im_data,cfg))
+    batch_gt_boxes.extend(gt_boxes)
+    batch_target_data.append(normalize_image(target1,cfg))
+    batch_target_data.append(normalize_image(target2,cfg))
+
+    #prep data for input to network
+    target_data = match_and_concat_images_list(batch_target_data,
+                                                min_size=cfg.MIN_TARGET_SIZE)
+    im_data = match_and_concat_images_list(batch_im_data)
+    gt_boxes = np.asarray(batch_gt_boxes) 
+    im_info = im_data.shape[1:]
+    im_data = np_to_variable(im_data, is_cuda=True)
+    im_data = im_data.permute(0, 3, 1, 2).contiguous()
+    target_data = np_to_variable(target_data, is_cuda=True)
+    target_data = target_data.permute(0, 3, 1, 2).contiguous()
+
+    print("hi")
   
