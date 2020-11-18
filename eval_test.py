cd5af301ad797f557ec91e4986989e744bffe2b9
Rasmus Lindahl <mrconter1@gmail.com>
2020-11-18 10:31:02 +0100

    Basic working version

--- eval_test.py
+++ eval_test.py
@@ -94,7 +94,31 @@ if __name__ == '__main__':
   net.cuda()
   net.eval()
 
-  print("Done")
+  batch_im_data = []
+  batch_target_data = []
+  batch_gt_boxes = []
+
+  im_data, gt_boxes, target1, target2 = load_image()
+
+  batch_im_data.append(normalize_image(im_data,cfg))
+  batch_gt_boxes.extend(gt_boxes)
+  batch_target_data.append(normalize_image(target1,cfg))
+  batch_target_data.append(normalize_image(target2,cfg))
+
+  #prep data for input to network
+  target_data = match_and_concat_images_list(batch_target_data,
+                                              min_size=cfg.MIN_TARGET_SIZE)
+  im_data = match_and_concat_images_list(batch_im_data)
+  gt_boxes = np.asarray(batch_gt_boxes) 
+  im_info = im_data.shape[1:]
+  im_data = np_to_variable(im_data, is_cuda=True)
+  im_data = im_data.permute(0, 3, 1, 2).contiguous()
+  target_data = np_to_variable(target_data, is_cuda=True)
+  target_data = target_data.permute(0, 3, 1, 2).contiguous()
+
+  scores, boxes = im_detect(net, target_data, im_data, im_info, features_given=False)
+
+  print(boxes[0])
 
 
 
